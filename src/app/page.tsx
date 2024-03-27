473454dbc73c80a4133b818959cc16243e41beeb
KyrianTM <r0929774@student.thomasmore.be>
2024-03-27 13:51:50 +0100

    test new page 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <Link href="/creator">
+        <Link href="/pages/creator">
             <a>
                 <Image
-                src="/images/TARON.jpg"
+                src="/TARON.jpg"
                 alt="Creator"
                 width={300}
                 height={300}
