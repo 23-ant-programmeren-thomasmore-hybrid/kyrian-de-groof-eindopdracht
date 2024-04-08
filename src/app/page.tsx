fbcb987edc3c236a3ffcee3fc8991d21b6eff7cc
KyrianTM <r0929774@student.thomasmore.be>
2024-04-08 15:18:21 +0200

    deploy attempt 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default async function Home() {
-    try {
-        const response = await fetch('/api/characterfetch');
-        if (response.ok) {
-            const data = await response.json();
-            return { userData: data };
-        } else {
-            console.error('Failed to fetch character data:', response.statusText);
-        }
-    } catch (error) {
-        console.error('Error fetching character data:', error);
-    }
+    await save();
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -22,4 +12,20 @@ export default async function Home() {
             </div>
         </main>
     );
+}
+
+export async function save() {
+    try {
+        const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000';
+        const response = await fetch(`${baseUrl}/api/characterfetch`);
+        if (response.ok) {
+            const data = await response.json();
+            return { userData: data };
+        } else {
+            console.error('Failed to fetch character data:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error fetching character data:', error);
+    }
+    return { userData: null };
 }
