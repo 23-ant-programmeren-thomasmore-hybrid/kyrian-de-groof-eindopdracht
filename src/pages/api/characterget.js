962bd39c74e1af01e1e1314a0419a2750befe49b
KyrianTM <r0929774@student.thomasmore.be>
2024-04-08 18:25:58 +0200

    characterget attempt 6

--- src/pages/api/characterget.js
+++ src/pages/api/characterget.js
@@ -38,9 +38,8 @@ export default async function handler(req, res) {
             });
         }
         catch (error) {
-
             console.error('Error fetching characters:', error);
-            res.status(500).json({ error: 'Error fetching characters' });
+            res.status(500).json({ error: `Error fetching characters: ${error.message}` });
         }
     }
     else {
