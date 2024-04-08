dd3ddb6b222ebd764514c3d1c78d097b642ef849
KyrianTM <r0929774@student.thomasmore.be>
2024-04-08 15:23:57 +0200

    deploy attempt 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default async function Home() {
-    await save();
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -14,18 +13,3 @@ export default async function Home() {
     );
 }
 
-async function save() {
-    try {
-        const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000';
-        const response = await fetch(`${baseUrl}/api/characterfetch`);
-        if (response.ok) {
-            const data = await response.json();
-            return { userData: data };
-        } else {
-            console.error('Failed to fetch character data:', response.statusText);
-        }
-    } catch (error) {
-        console.error('Error fetching character data:', error);
-    }
-    return { userData: null };
-}
--- src/pages/characters.tsx
+++ src/pages/characters.tsx
@@ -1,4 +1,5 @@
 export default async function Characters() {
+    await load();
 
     return (
         <main className="flex min-h-screen items-center justify-center bg-black">
@@ -10,7 +11,7 @@ export default async function Characters() {
     )
 }
 
-export async function save() {
+async function save() {
     try {
         const response = await fetch('/api/characterget');
         if (response.ok) {
@@ -23,5 +24,21 @@ export async function save() {
         console.error('Error fetching user data:', error);
     }
 
+    return { userData: null };
+}
+
+async function load() {
+    try {
+        const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000';
+        const response = await fetch(`${baseUrl}/api/characterfetch`);
+        if (response.ok) {
+            const data = await response.json();
+            return { userData: data };
+        } else {
+            console.error('Failed to fetch character data:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error fetching character data:', error);
+    }
     return { userData: null };
 }
