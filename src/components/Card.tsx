1c9ff039a7aaa0a533dba733642b3ec0a26b01b5
KyrianTM <r0929774@student.thomasmore.be>
2024-04-16 01:26:19 +0200

    characterget styling attempt 6

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 // @ts-ignore
 const Card = ({ character }) => {
     return (
-        <div className="bg-white m-2 rounded-lg shadow-md p-4 border-2 border-amber-950">
+        <div className="text-black bg-white m-2 rounded-lg shadow-md p-4 border-2 border-amber-950">
             <h2 className="text-xl font-bold">{character.name}</h2>
             <p className="text-gray-600">{character.nickname}</p>
             <p className="text-gray-700">{character.affiliation}</p>
