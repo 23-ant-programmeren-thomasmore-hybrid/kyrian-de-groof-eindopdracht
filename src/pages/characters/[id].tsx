960d66218da11cfd72fd4d693900cee9edecba57
KyrianTM <r0929774@student.thomasmore.be>
2024-04-16 03:47:05 +0200

    character detail 10

--- src/pages/characters/[id].tsx
+++ src/pages/characters/[id].tsx
@@ -11,14 +11,14 @@ export default function Character() {
     const parsedCharacter = Array.isArray(character) ? JSON.parse(character[0]) : JSON.parse(character || "null");
 
     return (
-        <div className="poster">
+        <div className="poster text-black">
             {parsedCharacter && (
                 <>
                     <h1>{parsedCharacter.name}</h1>
-                    <p>{parsedCharacter.nickname}</p>
-                    <p>{parsedCharacter.affiliation}</p>
-                    <p>{parsedCharacter.age}</p>
-                    <p>{parsedCharacter.allignment}</p>
+                    <span>{parsedCharacter.nickname}</span>
+                    <span>{parsedCharacter.affiliation}</span>
+                    <span>{parsedCharacter.age}</span>
+                    <span>{parsedCharacter.allignment}</span>
                 </>
             )}
         </div>
