6f337fab8af82c7d0b2cd439d94f030eeef25aa1
KyrianTM <r0929774@student.thomasmore.be>
2024-04-16 05:41:44 +0200

    pdf attempt 7

--- src/pages/characters/[id].tsx
+++ src/pages/characters/[id].tsx
@@ -95,10 +95,54 @@ const Character = () => {
                                 <dt className="text-sm font-medium text-gray-500">Name</dt>
                                 <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">{parsedCharacter.name}</dd>
                             </div>
-                            {/* Render other character details here */}
+                            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">Nickname</dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">{parsedCharacter.nickname}</dd>
+                            </div>
+                            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">title</dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">{parsedCharacter.title}</dd>
+                            </div>
+                            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">age</dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">{parsedCharacter.age}</dd>
+                            </div>
+                            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">Affiliation</dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">{parsedCharacter.affiliation}</dd>
+                            </div>
+                            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">Job</dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">{parsedCharacter.job}</dd>
+                            </div>
+                            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">Force Sensitive</dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">{parsedCharacter.forceSensitive ? "Yes" : "No"}</dd>
+                            </div>
+                            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">Species</dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">{parsedCharacter.species}</dd>
+                            </div>
+                            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">Homeworld</dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">{parsedCharacter.homeWorld}</dd>
+                            </div>
+                            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">Weapon</dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">{parsedCharacter.weapon}</dd>
+                            </div>
+                            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">Allignment</dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">{parsedCharacter.allignment}</dd>
+                            </div>
+                            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">Backstory</dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">{parsedCharacter.backstory}</dd>
+                            </div>
                         </dl>
                     </div>
-                    <button onClick={handleDownload} className="bg-gray-800 text-white p-2 rounded-md">Download PDF</button>
+                    <button onClick={handleDownload} className="bg-gray-800 text-white p-2 rounded-md">Download PDF
+                    </button>
                 </div>
             )}
         </div>
