d80465027be1704e65bd6192caa47a0e964056b1
KyrianTM <r0929774@student.thomasmore.be>
2024-04-16 02:14:38 +0200

    character detail 2

---
+++ src/pages/characters/[id].tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// @ts-ignore
+export default function Character({ character }) {
+    return (
+        <div className="poster">
+            <p className="text-black">test</p>
+        </div>
+    );
+}
